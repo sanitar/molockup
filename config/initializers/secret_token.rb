4305ffe7018361ffae999ea729f4b3a34204f5c8
Aleksandr Panasenkov <pan@applicot.ru>
2011-10-11 21:15:27 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Molockup::Application.config.secret_token = 'ee846656a190a790a25648af34b20a686a2115252206995ab5870ea79e444761dc1cf5c405a6dda0682f1a69a199d29212d96c301474be447aba450ecff78f3f'
